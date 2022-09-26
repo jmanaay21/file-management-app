a880a739a0a2e9fc5def697c74baf2c9521854c8
Andy <andy.gonzalez2@my.utsa.edu>
2022-09-26 15:40:33 -0500

    Added switch case statement for choosing which action to take on the server

---
+++ dev-work/secure.py
@@ -0,0 +1,50 @@
+"""
+This is a py file that will contain the functions used for security
+meaning LDAP implementation, encoding, decoding, hashing, etc.
+"""
+import base64
+
+class Secure:
+    """Classification for incuding sercurity functions"""
+    # def __init__(self):
+
+    def decodedData(self, b64EncodedData):
+        """Decode encoded data when received"""
+        decodedBytes = base64.b64decode(b64EncodedData)
+        print(decodedBytes)
+        with open("exampleSaveData.md", "w") as file:
+            file.write(decodedBytes.decode("UTF-8"))
+
+
+    def encodeFile(self, pathToFile):
+        """Encode file before sending it off to server"""
+        data = open(pathToFile, "r").read()
+        with open(pathToFile, "r") as file:
+            encoded = base64.b64encode(bytes(file.read(), "UTF-8"))
+            print(data)
+            print(encoded)
+            # decodedData(encoded) Make this function call work
+
+
+    # Press the green button in the gutter to run the script.
+    if __name__ == '__main__':
+        tempFilePath = "/home/parallels/Documents/file-management-app/README.md"
+        encodeFile(tempFilePath)
+
+    # def decodeFile(b64data):
+    #     decoded = base64.b64decode(b64data)
+    #     f = open('exampleSaveData.md', 'w')
+    #     f.write(decoded)
+    #     f.close()
+    #     print(decoded)
+
+    # def encodeFile(pathToFile):
+    #     data = open(pathToFile, 'r').read()
+    #     encoded = base64.b64encode(data)
+    #     print(data)
+    #     print(encoded)
+    #     decodeFile(encoded)
+
+    # if __name__ == "__main__":
+    #     tempfilepath = '/home/parallels/Documents/file-management-app/README.md'
+    #     encodeFile(tempfilepath)
---
+++ dev-work/test.py
@@ -0,0 +1,98 @@
+"""
+This test is just an internal example of how each
+function would be laid out and work within the server
+as well as how the encoding and decoding might be laid out
+
+TO DO:
+- Move encode and decode to seperate file
+    (maybe even a seperate class)
+- Add different users/ access control
+- Implement encode and decode functions to
+    add to file tables from python script
+"""
+import base64
+from getpass import getpass
+
+from mysql.connector import Error, connect
+
+
+def main():
+    """Main function to run"""
+    try:
+        with connect(
+            # Setting up connection to server database
+            host="localhost",
+            user=input("Enter username: "),
+            password=getpass("Enter password: "),
+            database="fileserver"
+        ) as connection:
+            with connection.cursor() as cursor:
+                # Moves to the fileserver table
+                cursor.execute("USE fileserver")
+
+                # Describes the contents of file_store table
+                # and displays information
+                cursor.execute("DESCRIBE file_store")
+                myresult = cursor.fetchall()
+
+                for x in myresult:
+                    print(x)
+
+                # Get user info to prepare to uplaod data into server
+                user_args = int(input("\nWhat would you like to do?\n"
+                + "1 for upload, 2 for download, 3 for delete: "))
+
+                # Choose action for database
+                database_action(user_args)
+
+    except Error as e:
+        print(e)
+
+
+def file_info():
+    """Get user info and store it for mysql use"""
+    file_name = input("\nInsert file name: ")
+    file_ext = input("Insert file extension: ")
+    file_ext = "." + file_ext
+    file_path = input("Insert file path to encode and upload:\n")
+
+    print('\nHere is your file staged to upload:')
+    print(f'file: {file_name}{file_ext}\n'
+        + f'file path: {file_path}')
+
+# Function to decide whether to upload or delete from Database
+def database_action(user_args):
+    """Case function decides to up, down or delete, 
+    once case is decided, user access should be checked"""
+    match user_args:
+        case 1:
+            print('option 1, upload:')
+            file_info()
+
+        case 2:
+            return 'option 2, download:'
+
+        case 3:
+            return 'option 3, delete:'
+
+
+# Refactor this to input path file to upload
+def decodedData(b64EncodedData):
+    """Decode encoded data when received"""
+    decodedBytes = base64.b64decode(b64EncodedData)
+    print(decodedBytes)
+    with open("exampleSaveData.md", "w") as file:
+        file.write(decodedBytes.decode("UTF-8"))
+
+
+def encodeFile(file_path):
+    """Encode file before sending it off to server"""
+    data = open(file_path, "r").read()
+    with open(file_path, "r") as file:
+        encoded = base64.b64encode(bytes(file.read(), "UTF-8"))
+        print(data)
+        print(encoded)
+        decodedData(encoded)
+
+if __name__ == '__main__':
+    main()

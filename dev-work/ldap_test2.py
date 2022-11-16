7f281630f0d4d09323e44583fc9d0ff9b0211eea
Andy Gonzalez <andy.gonzalez2@my.utsa.edu>
2022-11-16 14:45:06 -0600

    Fixed merge issues

--- dev-work/ldap_test2.py
+++ dev-work/ldap_test2.py
@@ -1,6 +1,5 @@
 import ldap3
 
-<<<<<<< HEAD
 #Stuff needed in beginning of the code
 LDAP_SERVER = 'ldap://fileserver.net'
 BASE_DN = 'dc=fileserver,dc=net'  # base dn to search in
@@ -26,16 +25,3 @@ def ldapConnection(LDAP_LOGIN, LDAP_PASSWORD):
     connect.set_option(ldap.OPT_REFERRALS, 0)  # to search the object and all its descendants
     result = connect.search_s(BASE_DN, ldap.SCOPE_SUBTREE, OBJECT_TO_SEARCH, ATTRIBUTES_TO_SEARCH)  #searches through ldap
     return result   #This needs to return a group id so we can use it for authorization in the code
-=======
-LDAP_SERVER = 'ldap://fileserver.net'
-BASE_DN = 'dc=fileserver,dc=net'  # base dn to search in
-LDAP_LOGIN = 'ldap_login'
-LDAP_PASSWORD = 'ldap_password'
-OBJECT_TO_SEARCH = 'userPrincipalName=user@example.com'
-ATTRIBUTES_TO_SEARCH = ['memberOf']
-
-connect = ldap3.initialize(LDAP_SERVER)
-connect.set_option(ldap3.OPT_REFERRALS, 0)  # to search the object and all its descendants
-connect.simple_bind_s(LDAP_LOGIN, LDAP_PASSWORD)
-result = connect.search_s(BASE_DN, ldap3.SCOPE_SUBTREE, OBJECT_TO_SEARCH, ATTRIBUTES_TO_SEARCH)
->>>>>>> 5234ba0 (mild main changes to function)

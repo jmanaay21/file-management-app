03918d7352ad2bbe3e0b52e70c46514350d4e01e
Devin Pockrus <devinmac@Devins-MacBook-Air.local>
2022-11-16 08:48:31 -0600

    Some new ideas working with the second file

--- dev-work/ldap_test2.py
+++ dev-work/ldap_test2.py
@@ -1,13 +1,27 @@
 import ldap
 
-LDAP_SERVER = 'ldap://example.com'
-BASE_DN = 'dc=example,dc=com'  # base dn to search in
-LDAP_LOGIN = 'ldap_login'
-LDAP_PASSWORD = 'ldap_password'
-OBJECT_TO_SEARCH = 'userPrincipalName=user@example.com'
-ATTRIBUTES_TO_SEARCH = ['memberOf']
+#Stuff needed in beginning of the code
+LDAP_SERVER = 'ldap://fileserver.net'
+BASE_DN = 'dc=fileserver,dc=net'  # base dn to search in
 
-connect = ldap.initialize(LDAP_SERVER)
-connect.set_option(ldap.OPT_REFERRALS, 0)  # to search the object and all its descendants
-connect.simple_bind_s(LDAP_LOGIN, LDAP_PASSWORD)
-result = connect.search_s(BASE_DN, ldap.SCOPE_SUBTREE, OBJECT_TO_SEARCH, ATTRIBUTES_TO_SEARCH)
+#login for username and password, to be passed into the ldapConnection function
+LDAP_LOGIN = input('Enter your ldap login: ')
+LDAP_PASSWORD = input('Enter your ldap password: ')
+
+def ldapConnection(LDAP_LOGIN, LDAP_PASSWORD):
+    try:
+        connect = ldap.initialize(LDAP_SERVER)  #connects to server
+        print("Connection initialized")
+    except ldap.SERVER_DOWN:
+        print("Can't connect to LDAP server")
+        exit(4)
+    try:
+        connect.simple_bind_s(LDAP_LOGIN, LDAP_PASSWORD)    #login for user
+    except (ldap.INVALID_CREDENTAILS):
+        print("Invalid login and/or password")
+        exit(3)
+    ATTRIBUTES_TO_SEARCH = ['memberOf']
+    OBJECT_TO_SEARCH = 'userPrincipalName=user@example.com'
+    connect.set_option(ldap.OPT_REFERRALS, 0)  # to search the object and all its descendants
+    result = connect.search_s(BASE_DN, ldap.SCOPE_SUBTREE, OBJECT_TO_SEARCH, ATTRIBUTES_TO_SEARCH)  #searches through ldap
+    return result   #This needs to return a group id so we can use it for authorization in the code
